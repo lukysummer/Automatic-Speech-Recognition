ebd8d5bb5fda1396a97ac9e769841672bfb1adc2
Lucrece Shin <lucrece.shin@utoronto.ca>
2019-03-21 13:03:31 -0400

    made functions more concise

--- char_map.py
+++ char_map.py
@@ -1,68 +1,67 @@
 """ Defines two dictionaries for converting between text and integer sequences. """
 
-char_map_str = """
-' 0
-<SPACE> 1
-a 2
-b 3
-c 4
-d 5
-e 6
-f 7
-g 8
-h 9
-i 10
-j 11
-k 12
-l 13
-m 14
-n 15
-o 16
-p 17
-q 18
-r 19
-s 20
-t 21
-u 22
-v 23
-w 24
-x 25
-y 26
-z 27
-"""
+char_int_pairs = """
+                    ' 0
+                    <SPACE> 1
+                    a 2
+                    b 3
+                    c 4
+                    d 5
+                    e 6
+                    f 7
+                    g 8
+                    h 9
+                    i 10
+                    j 11
+                    k 12
+                    l 13
+                    m 14
+                    n 15
+                    o 16
+                    p 17
+                    q 18
+                    r 19
+                    s 20
+                    t 21
+                    u 22
+                    v 23
+                    w 24
+                    x 25
+                    y 26
+                    z 27
+                 """
 
-char_map = {}
-index_map = {}
+char_to_int = {}
+int_to_char = {}
 
-for line in char_map_str.strip().split('\n'):
-    ch, index = line.split()
-    char_map[ch] = int(index)
-    index_map[int(index) + 1] = ch
+for line in char_int_pairs.strip().split('\n'):
+    ch, _int = line.split()
+    char_to_int[ch] = int(_int)
+    int_to_char[int(_int) + 1] = ch
     
-index_map[2] = ' '
+int_to_char[2] = ' '  # modify <SPACE> to ' '
+    
 
-print(char_map)
-print()
-print(index_map)
 
 def text_to_int(text):
-    """ Convert text to an integer sequence """
+    """ Convert text into an integer sequence """
+    
     int_sequence = []
-    for c in text:
-        if c == ' ':
-            ch = char_map['<SPACE>']
+    for ch in text:
+        if ch == " ":
+            int_sequence.append(char_to_int['<SPACE>'])
         else:
-            ch = char_map[c]
-        int_sequence.append(ch)
+            int_sequence.append(char_to_int[ch])
         
     return int_sequence
 
 
+
 def int_to_text(int_sequence):
-    """ Convert an integer sequence to text """
+    """ Convert integer sequence into text """
+    
     text = []
-    for c in int_sequence:
-        ch = index_map[c]
-        text.append(ch)
+    for _int in int_sequence:
+        text.append(int_to_char[_int])
         
     return text
